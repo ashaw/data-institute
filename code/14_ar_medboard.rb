31efb106eca15b7a17d48f62ddd546dcc4f6994d
Al Shaw <almshaw@gmail.com>
2016-05-10 12:50:16 -0400

    ar medboard scraper

---
+++ code/14_ar_medboard.rb
@@ -0,0 +1,11 @@
+require 'nokogiri'
+require 'restclient'
+
+doc = Nokogiri::HTML(RestClient.get("https://goo.gl/evoIpq"))
+table = doc.css("#ctl00_ctl00_MainContentPlaceHolder_innercontent_gvLicNotices")
+out = ""
+out << table.css("th").map(&:text).join(",") + "\n"
+table.css("tr")[1..-1].each do |row|
+  out << row.css("td").map(&:text).join(",") + "\n"
+end
+File.open("ar_medboard.csv", "w") {|f| f.write out }
